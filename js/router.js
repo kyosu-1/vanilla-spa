dc8392dc634f829a20fcdd7e26a3843c74b97c8f
kyosu-1 <sho013039@gmail.com>
2023-07-16 09:29:53 +0900

    feat: add jsdoc to router.js

--- js/router.js
+++ js/router.js
@@ -1,9 +1,8 @@
 /**
+ * Handles routing for SPA by updating the browser's history and changing the page content.
  * 
- * @param {*} event 
- *
+ * @param {Event} [event] - The event object associated with the click (or equivalent) event. Defaults to window.event.
  */
-
 const route = (event) => {
     event = event || window.event;
     event.preventDefault();
@@ -11,6 +10,11 @@ const route = (event) => {
     handleLocation();
 };
 
+/**
+ * Routes object, defines the path for each route in the SPA.
+ *
+ * @type {Object.<string, string>}
+ */
 const routes = {
     404: "/pages/404.html",
     "/": "/pages/index.html",
@@ -18,6 +22,10 @@ const routes = {
     "/lorem": "/pages/lorem.html",
 };
 
+/**
+ * Asynchronously fetches and loads the appropriate HTML file based on the current URL path.
+ * If the path does not match any route in the routes object, it defaults to the 404 page.
+ */
 const handleLocation = async () => {
     const path = window.location.pathname;
     const route = routes[path] || routes[404];
@@ -25,7 +33,11 @@ const handleLocation = async () => {
     document.getElementById("main-page").innerHTML = html;
 };
 
+// Assign handleLocation function to onpopstate event of the window object
 window.onpopstate = handleLocation;
+
+// Assign route function to the global scope
 window.route = route;
 
+// Call handleLocation function to load the correct content when the page first loads
 handleLocation();
